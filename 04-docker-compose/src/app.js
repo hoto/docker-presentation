412f6c66027a9597284d22e0effc2b1760c22634
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-01-17 22:35:18 +0100

    working connection to mongo in 04 example

--- 04-docker-compose/src/app.js
+++ 04-docker-compose/src/app.js
@@ -1,6 +1,22 @@
 const Express = require('express')
 const app = Express()
 
+const MongoClient = require('mongodb').MongoClient
+
+let db;
+MongoClient
+  .connect('mongodb://mongo:27017', (err, database) => {
+    if (err) return console.log('Not connected to mongo yet...', err)
+    console.log('Connected to mongo!')
+    db = database
+  })
+
 app
-  .get('/', (req, res) => res.json({msg: 'Hello from nodejs app!'}))
+  .get('/', (req, res) => res.json(
+    {
+      msg: 'Hello from nodejs app!',
+      isDbConnected: !!db
+    }
+  ))
   .listen(8080, console.log('Listening on localhost:8080'))
+
